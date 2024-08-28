61f448e4bc8d7edfc32ec843f3097ddd73171b61
namng <nllsover8.0@gmail.com>
2024-08-28 09:33:27 +0700

    chore: delete typo error class

--- src/main/java/com/example/health_management/domain/repositories/ApppointmentTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.health_management.domain.repositories;
-
-import com.example.health_management.domain.entities.ApppointmentType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApppointmentTypeRepository extends JpaRepository<ApppointmentType, Integer> {
-}
