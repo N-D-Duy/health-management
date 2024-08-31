688ecee6d40a7dbc396ba686d6ed553d9408bd2c
namng <nllsover8.0@gmail.com>
2024-08-31 22:19:20 +0700

    fix: delete error prone imports

--- src/main/java/com/example/health_management/common/config/SwaggerConfig.java
+++ src/main/java/com/example/health_management/common/config/SwaggerConfig.java
@@ -1,25 +1,11 @@
 package com.example.health_management.common.config;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.bind.Name;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @Configuration
 @OpenAPIDefinition
         (info = @Info(contact = @Contact(name = "Health Management Center"), version = "1.0.0", description = "API for testing request", title = "Health Management"))
